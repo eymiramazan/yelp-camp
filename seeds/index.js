668122f56b9d41eaa74abf816ab78014560c1f95
eymiramazan <ramazaneymir@gmail.com>
2021-03-06 23:04:00 +0300

    add cluster map and change styles

--- seeds/index.js
+++ seeds/index.js
@@ -20,7 +20,7 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 300; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
@@ -31,7 +31,11 @@ const seedDB = async () => {
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer risus neque, mollis ac leo ac, semper ornare libero. Aliquam tempor",
       price: price,
       geometry: { 
-        type: 'Point', coordinates: [ 28.3251, 37.91584 ] 
+        type: 'Point', 
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude
+        ]
       },
       images: [
         {
