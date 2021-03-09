cb99ee5912755c9fdda17358ce80e1e4f3fcd1c6
eymiramazan <ramazaneymir@gmail.com>
2021-03-09 15:13:36 +0300

    add start script

--- app.js
+++ app.js
@@ -154,6 +154,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err });
 });
 
-app.listen(3000, () => {
-  console.log("Server Running on Port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server Running on Port ${port}`);
 });
